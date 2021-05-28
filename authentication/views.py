6042edf1ed00c5c729219512d8e0642d1f8726a7
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-28 14:36:15 +0300

    add signup, login and signout views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.contrib.auth import get_user_model
+from django.views.generic import View
+
+User = get_user_model()
+
 
 # Create your views here.
+class SignUp(View):
+    """
+    Sign up view
+    """
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Login(View):
+    """
+    Sign in view
+    """
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class SignOut(View):
+    """
+    Sign out view
+    """
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
