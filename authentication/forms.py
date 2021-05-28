9c7423eeef015ed6f27c7994c035331996c090cb
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-28 16:49:29 +0300

    add login form and view

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,12 +1,25 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
 # Authentication forms
-class UserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('full_name', 'username', 'email', 'password1', 'password2')
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "id": "user_password"
+            }
+        )
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        user_qs = User.objects.filter(username__iexact=username)
+
+        if not user_qs.exists:
+            raise forms.ValidationError("This is an invalid user.")
+            return username
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.views.generic import View
+from .forms import LoginForm
+from django.contrib import messages
 
 User = get_user_model()
 
@@ -23,12 +25,28 @@ class Login(View):
     """
     Sign in view
     """
+    template_name = 'authentication/signin.html'
+    form = LoginForm
 
     def get(self, request, *args, **kwargs):
-        pass
+        if request.user.is_authenticated:
+            return redirect('home_view')
 
     def post(self, request, *args, **kwargs):
-        pass
+        form = self.form(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is None:
+                messages.error(request, "Invalid Login", extra_tags="error")
+                return render(request, self.template_name)
+
+            login(request, user)
+            messages.success(request, f"Welcome {user.username}", extra_tags="success")
+            return redirect('home_view')
 
 
 class SignOut(View):
