5929c80052ccf0ec849e4b98a7cf91058236fcee
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-28 20:47:45 +0300

    update the login and sigup view

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.views.generic import View
-from .forms import LoginForm, SignOutForm
+from .forms import RegistrationForm
 from django.contrib import messages
 
 User = get_user_model()
@@ -14,7 +14,7 @@ class SignUp(View):
     """
     title = 'signup'
 
-    form = SignOutForm
+    form = RegistrationForm
     template_name = 'authentication/signup.html'
 
     def get(self, request, *args, **kwargs):
@@ -22,29 +22,26 @@ class SignUp(View):
             return redirect('home_view')
 
     def post(self, request, *args, **kwargs):
-        form = self.form(request.post or None)
-        if form.is_valid():
-            username = form.cleaned_data("username")
-            email = form.cleaned_data("email")
-            password = form.cleaned_data("password1")
-            password2 = form.cleaned_data("password2")
+        form = self.form()
+        if request.method == 'POST':
+            form = form(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login_view')
 
-            user = User.objects.create_user(username, email, password)
+        else:
+            form = form()
 
-            return redirect('signin_view')
-
-        context = {"title": self.title, "user": self.user}
-
+        context = {"form": form, "title": self.title}
         return render(request, self.template_name, context)
 
 
 class Login(View):
     """
-    Sign in view
+    Login view
     """
     title = 'Login'
 
-    form = LoginForm
     template_name = 'authentication/signin.html'
     context = {"title", title}
 
@@ -58,7 +55,6 @@ class Login(View):
         context = self.context
 
         if request.method == 'POST':
-            form = self.form(request.POST)
             username = request.POST.get('username')
             password = request.POST.get('password')
             if username and password:
