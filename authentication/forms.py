b04a03221952f0ed6210e76ad996343ed0c6f281
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-28 17:05:27 +0300

    add sign up form

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,25 +1,71 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from core.models import Profile
 
 User = get_user_model()
 
 
 # Authentication forms
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30)
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control",
-                "id": "user_password"
-            }
+    class Meta:
+        model = Profile
+
+        username = forms.CharField(max_length=30)
+        password = forms.CharField(
+            widget=forms.PasswordInput(
+                attrs={
+                    "class": "form-control",
+                    "id": "user_password"
+                }
+            )
         )
-    )
 
-    def clean_username(self):
-        username = self.cleaned_data.get("username")
-        user_qs = User.objects.filter(username__iexact=username)
+        def clean_username(self):
+            username = self.cleaned_data.get("username")
+            user_qs = User.objects.filter(username__iexact=username)
 
-        if not user_qs.exists:
-            raise forms.ValidationError("This is an invalid user.")
-            return username
+            if not user_qs.exists:
+                raise forms.ValidationError("This is an invalid user.")
+                return username
+
+
+class SignOutForm(forms.Form):
+    class Meta:
+        model: Profile
+
+        username = forms.CharField(max_length=30)
+        email = forms.EmailField(required=True)
+        password1 = forms.CharField(
+            widget=forms.PasswordInput(
+                label='Password',
+                attrs={
+                    "class": "form-control",
+                    "id": "user_password"
+                }
+            )
+        )
+        password2 = forms.CharField(
+            widget=forms.PasswordInput(
+                label='Confirm Password',
+                attrs={
+                    "class": "form-control",
+                    "id": "user_password"
+                }
+            )
+        )
+
+        def clean_username(self):
+            username = self.cleaned_data.get("username")
+            user_qs = User.objects.filter(username__iexact=username)
+
+            if user_qs.exists:
+                raise forms.ValidationError("This username is already taken.")
+                return username
+
+        def clean_email(self):
+            email = self.cleaned_data.get("email")
+            user_qs = User.objects.filter(email__iexact=email)
+
+            if user_qs.exists:
+                raise forms.ValidationError("This email has a registered account")
+                return username
